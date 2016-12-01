3865e7cf162a736af2341a96b1a88de91c448176
WillyLu326 <luzh0326@gmail.com>
2016-12-01 13:09:04 -0500

    update delete

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const express = require('express'),
 
 module.exports = router;
 
-const data = [
+var data = [
 	{username: 'Willy', password: '123'},
 	{username: 'Zheng', password: '321'}
 ];
@@ -15,4 +15,21 @@ router.post('/data', function(req, res) {
 	const body = req.body;
 	data.push(body);
 	res.send('Done');
+});
+router.put('/data/:name', function(req, res) {
+	const body = req.body;
+	const name = req.params.name;
+
+	data.map(obj => {
+		if (obj.username == name) {
+			obj.password = body.password;
+			return res.send('Done');
+		}
+ 	});
+ 	return res.send('No this value');
+});
+router.delete('/data/:name', function(req, res) {
+	const name = req.params.name;
+	data = data.filter(obj => obj.username != name);
+	return res.send('Success delete');
 });
